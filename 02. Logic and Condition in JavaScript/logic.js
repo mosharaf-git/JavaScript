0972236fcc07cf5528d56fc035b6c46718416c8e
mosharaf-h <mosharaf.dev@gmail.com>
2021-09-20 17:26:11 +0600

    all files

--- 02. Logic and Condition in JavaScript/logic.js
+++ 02. Logic and Condition in JavaScript/logic.js
@@ -1,9 +1,67 @@
-// If Else Condition in JavaScript
-var a = 10
-var b = 20
+// If Else if Else Condition in JavaScript
+// var a = 10
+// var b = 20
+// var c = 30
 
-if(a < b){
-    console.log("This Statement is True")
-}else{
-    console.log("Nope! This Statement is False")
+// if(a > b){
+//     console.log("(a < b) This Statement is True")
+// }else if(c > a){
+//     console.log("(c > a) This Statement is also True")
+// }else{
+//     console.log("Nope! This Statement is False")
+// }
+
+
+// Switch Statement
+var date = new Date()
+var today = date.getDay()
+
+switch (today) {
+    case 0:
+        console.log('Today is Sunday')
+        break
+
+    case 1:
+        console.log('Today is Monday')
+        break
+
+    case 2:
+        console.log('Today is Tuesday')
+        break
+
+    case 3:
+        console.log('Today is Wednesday')
+        break
+    case 4:
+        console.log('Today is Thursday')
+        break
+
+    case 5:
+        console.log('Today is Friday')
+        break
+
+    case 6:
+        console.log('Today is Saturday')
+        break
+
+    default:
+        console.log('Enter Valid Number!!')
+        break
 }
+
+// Ternary Operator
+var n = 10
+var result = n%2 == 0 ? 'Even' : 'Odd'
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
