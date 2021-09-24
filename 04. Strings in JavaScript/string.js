de5123e4f11bf6ca058f52cd9643dd76fe0833a5
mosharaf-h <mosharaf.dev@gmail.com>
2021-09-24 10:23:12 +0600

    string chapter completed

--- 04. Strings in JavaScript/string.js
+++ 04. Strings in JavaScript/string.js
@@ -1,49 +1,48 @@
-// // Strings in JavaScript
-// var str = 'I Love JS'                           // String Literal
-// console.log(str)
+// Strings in JavaScript
+var str = 'I Love JS'                           // String Literal
+console.log(str)
 
-// var str1 = String('I Love Python Too')          // String Constructor
-// console.log(str1)       
+var str1 = String('I Love Python Too')          // String Constructor
+console.log(str1)       
 
-// var n = 420
-// var nToString = n + ''
-// var nConvert = n.toString()                     // Type Conversion
-// console.log(nToString)
-// console.log(nConvert)
+var n = 420
+var nToString = n + ''
+var nConvert = n.toString()                     // Type Conversion
+console.log(nToString)
+console.log(nConvert)
 
 // Escape Notation
-// var value = '\tMosharaf Hossain\n'             // Tab & New Line 
-// var number =  4234
+var value = '\tMosharaf Hossain\n'             // Tab & New Line 
+var number =  4234
 
-// console.log(value)
-// console.log(number)
+console.log(value)
+console.log(number)
 
 // String Comparison
-// var a = 'a'
-// var b = 'B'                                     // JavaScript Lexicographic System
-// console.log(a > b)
+var a = 'a'
+var b = 'B'                                     // JavaScript Lexicographic System
+console.log(a > b)
 
 // String Method
-// var firstName = 'Mosharaf'
-// var lastName = 'Hossain'
-// var finalName = firstName.concat(' ', lastName)     // Concat Method
-// console.log(finalName)
+var firstName = 'Mosharaf'
+var lastName = 'Hossain'
+var finalName = firstName.concat(' ', lastName)     // Concat Method
+console.log(finalName)
 
-// console.log(finalName.toUpperCase())                // Uppercase Method
-// console.log(finalName.toLowerCase())                // Uppercase Method
+console.log(finalName.toUpperCase())                // Uppercase Method
+console.log(finalName.toLowerCase())                // Uppercase Method
 
-// console.log(finalName.substr(0, 11))                // Substring Method
-// console.log(firstName.charAt(3))                    // Character At Method
+console.log(finalName.substr(0, 11))                // Substring Method
+console.log(firstName.charAt(3))                    // Character At Method
+console.log(finalName.startsWith('Mosharaf'))       // Starts With Method
+console.log(finalName.endsWith('Hossain'))          // Ends With Method
 
-// console.log(finalName.startsWith('Mosharaf'))       // Starts With Method
-// console.log(finalName.endsWith('Hossain'))          // Ends With Method
+var space = '       Hello World       '
+console.log(space.trim())                           // To Trim Leading and Trailing Space
+console.log(space.trimStart())                     // To Trim Leading Space
+console.log(space.trimEnd())                      // To Trim Trailing Space
 
-// var space = '       Hello World       '
-// console.log(space.trim())                           // To Trim Leading and Trailing Space
-// console.log(space.trimStart())                     // To Trim Leading Space
-// console.log(space.trimEnd())                      // To Trim Trailing Space
-
-var splitting = 'Mosharaf Hossain'
+var splitting = 'Mosharaf Hossain'               // Split a String
 console.log(splitting.split(' '))
 
 
@@ -66,9 +65,5 @@ if(length > 15){
 }else{
     console.log('Welcome Home')
 }
-
-
-
-
 
 
