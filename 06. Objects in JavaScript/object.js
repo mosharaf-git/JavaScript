9f5733138b19a2f57a6842fcde12511ff43e13aa
mosharaf-h <mosharaf.dev@gmail.com>
2021-09-25 11:05:06 +0600

    Object Files

---
+++ 06. Objects in JavaScript/object.js
@@ -0,0 +1,8 @@
+var obj = {
+    myName: 'Mosharaf Hossain',
+    myRoll: 366866,
+    myInstitute: 'Sherpur Polytechnic Institute',
+    myOccupation: 'Junior Software Developer'
+}
+
+console.log(`Meet Mr. ${obj.myName} Who is a student of ${obj.myInstitute} and He is now ${obj.myOccupation} in Brain Station 23`)
