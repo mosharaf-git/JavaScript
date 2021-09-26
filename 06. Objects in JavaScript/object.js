421b5917062b59c4c93eb465c6ffed5b0e2f4bce
mosharaf-h <mosharaf.dev@gmail.com>
2021-09-26 07:04:10 +0600

    Object

--- 06. Objects in JavaScript/object.js
+++ 06. Objects in JavaScript/object.js
@@ -1,25 +1,39 @@
 // Object Literal in Javascript
-var obj = {
-    myName: 'Mosharaf Hossain',
-    myRoll: 366866,
-    myInstitute: 'Sherpur Polytechnic Institute',
-    myOccupation: 'Junior Software Developer'
-}
+// var obj = {
+//     myName: 'Mosharaf Hossain',
+//     myRoll: 366866,
+//     myInstitute: 'Sherpur Polytechnic Institute',
+//     myOccupation: 'Junior Software Developer'
+// }
 
-obj.myName = 'Musharraf Hussein'        // Setting Object Property
+// obj.myName = 'Musharraf Hussein'        // Setting Object Property
 
-console.log(obj.myInstitute)            // Access Object Property
+// console.log(obj.myInstitute)            // Access Object Property
 
-delete obj.myRoll                       // Remove Object Property
-console.log(obj)
+// delete obj.myRoll                       // Remove Object Property
+// console.log(obj)
 
-console.log("Meet Mr. "+obj.myName+ " who is a student of "+ obj.myInstitute +" and His roll no is "+ obj.myRoll +" he is now "+ obj.myOccupation +" in Brain Station 23")
+// console.log("Meet Mr. "+obj.myName+ " who is a student of "+ obj.myInstitute +" and His roll no is "+ obj.myRoll +" he is now "+ obj.myOccupation +" in Brain Station 23")
+
 
 // Object Constructor in JavaScript
-var myObject = new Object()
-myObject.name = 'Mosharaf Hossain'
-console.log(myObject.name)
+// var myObject = new Object()
+// myObject.name = 'Mosharaf Hossain'
+// console.log(myObject.name)
 
+
+// Object Iteration
+var myObject2 = {
+    x: 10,
+    y: 30,                          // x, y, z is key/property and 10, 30, 50 is value
+    z: 50
+}
+console.log('x' in myObject2)
+
+for(var i in myObject2){
+    console.log(i)
+    console.log(myObject2[i])
+}
 
 
 
