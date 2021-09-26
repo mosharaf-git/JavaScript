a5003144f8dda904aeb49932b46fc96455bcfbea
mosharaf-h <mosharaf.dev@gmail.com>
2021-09-26 07:18:22 +0600

    JS Object Completed

--- 06. Objects in JavaScript/object.js
+++ 06. Objects in JavaScript/object.js
@@ -23,32 +23,31 @@
 
 
 // Object Iteration
-var myObject2 = {
-    x: 10,
-    y: 30,                          // x, y, z is key/property and 10, 30, 50 is value
-    z: 50
+// var myObject2 = {
+//     x: 10,
+//     y: 30,                          // x, y, z is key/property and 10, 30, 50 is value
+//     z: 50
+// }
+// console.log('x' in myObject2)
+
+// for(var i in myObject2){
+//     console.log(i)
+//     console.log(myObject2[i])
+// }
+
+
+// Object Methods
+var myObject3 = {
+    x: 90,
+    y: 180,
+    z: 270
 }
-console.log('x' in myObject2)
+console.log(Object.keys(myObject3))             // Print the keys of an object
+console.log(Object.values(myObject3))           // Print the value of an object
+console.log(Object.entries(myObject3))          // Print array of an object
 
-for(var i in myObject2){
-    console.log(i)
-    console.log(myObject2[i])
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var myObject4 = Object.assign({}, myObject3)    // Clone and Create an new object
+myObject4.x = 200
+console.log(myObject4)
 
 
